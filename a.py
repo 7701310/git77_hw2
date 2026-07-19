0e62871428c2e6714e41c715f58be6d73d286c3d
12 <a0981075285@gmail.com>
2026-07-19 18:34:45 +0800

    name

--- a.py
+++ a.py
@@ -1,6 +1,17 @@
-a=123
+# 記得先裝flask (pip install flask)
+from flask import Flask, jsonify
 
-b=113131312
-print(a+b)
-print(b)
+app = Flask(__name__)
+app.json.ensure_ascii = False
+name = "謝士宏"
 
+
+@app.get("/api/name")
+def get_name():
+    return jsonify({
+        "name": name
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
